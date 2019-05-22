01ad636ac9f83cdd4ad607581ad2c0f212611fee
Ann MB <37158166+ANN-MB@users.noreply.github.com>
2019-05-23 00:45:57 +0200

    Update absolute-clone.js

--- absolute-clone.js
+++ absolute-clone.js
@@ -1,39 +1,43 @@
-	// Create a clone of an element
-	// with exacts same appearance
-	// and position but appended in
-	// BODY and "position:absolute"
-  
-	function getCssText(a) {
-	  var b = window.getComputedStyle(a), c;
-	  if ("" != b.cssText) { return b.cssText; }
-	  c = "";
-	  for (var i = 0; i < b.length; i++) {
-	    c += b[i] + ":" + b.getPropertyValue(b[i]) + ";";
-	  }
-	  return c;
-	}
+// Create a clone of an element
+// with exacts same appearance
+// and position but appended in
+// BODY and "position:absolute"
 
-	function absClone(a) {
-	  var b = a.cloneNode(true),
-	      c = a.getBoundingClientRect(),
-	      d = c.left + (window.pageXOffset || document.documentElement.scrollLeft) + "px",
-	      e = c.top + (window.pageYOffset || document.documentElement.scrollTop) + "px";
-	  b.style.cssText = getCssText(a);
-	  b.style.position = "absolute";
-	  b.style.left = d;
-	  b.style.top = e;
-	  b.zIndex = "" != a.zIndex ? +a.zIndex + 1 : 16777271;
-	  b.id = (a.id) ? a.id + "_cloned" : "";
-	  return b
-	}
+function absClone(a) {
+  var b = a.cloneNode(true),
+    c = a.getBoundingClientRect(),
+    d = c.left + (window.pageXOffset || document.documentElement.scrollLeft) + "px",
+    e = c.top + (window.pageYOffset || document.documentElement.scrollTop) + "px",
+    f = window.getComputedStyle(a),
+    g;
+  if ("" != f.cssText) {
+    b.style.cssText = f.cssText;
+  } else {
+    g = "";
+    for (var i = 0; i < f.length; i++) {
+      g += f[i] + ":" + f.getPropertyValue(f[i]) + ";";
+    }
+    b.style.cssText = g
 
-	var element = document.getElementById("ahah"); // element to be cloned
-	var myClone = absClone(element);               // creating clone and assign it a variable 
+  }
+  b.style.position = "absolute";
+  b.style.left = d;
+  b.style.top = e;
+  b.zIndex = "" != a.zIndex ? +a.zIndex + 1 : 16777271;
+  b.id = (a.id) ? a.id + "_cloned" : "";
+  return b
+}
 
-	// do whatever you want to do with "myClone", for example :
+/************************** USAGE **************************/
 
-	myClone.style.border = "1px solid #F00"
+// element to be cloned :
+var element = document.getElementById("ahah");   
 
-	// Then, append the clone :
+// creating clone and assign it a variable
+var myClone = absClone(element);                 
 
-	document.body.appendChild(myClone);
+// do whatever you want to do with "myClone", for example :
+myClone.style.border = "1px solid #F00"
+
+// Then, append the clone :
+document.body.appendChild(myClone);
