5eae0490cfe3855dea3177f22b090d7ac7ddada0
4nnm8 <37158166+4nnm8@users.noreply.github.com>
2024-04-03 01:32:27 +0000

    Update generateElement.js

--- generateElement.js
+++ generateElement.js
@@ -1,10 +1,10 @@
 const generateElement = (p) => {
   let n = document.createElement(p.tag);
-  p.classes && (n.className = p.classes);
+  p.classList && (n.className = p.classList);
   p.style && (n.style.cssText = p.style);
   p.content && (n.innerHTML = p.content);
   p.appendIn && p.appendIn.appendChild(n);
-  p.attribs && Object.entries(p.attribs).forEach((a) => { n.setAttribute(y[0], y[1]) });
+  p.attributes && Object.entries(p.attributes).forEach((a) => { n.setAttribute(y[0], y[1]) });
   p.events && Object.entries(p.events).forEach((a) => { n.addEventListener(y[0], y[1], !1) });
   return n
 };
@@ -13,11 +13,11 @@ const generateElement = (p) => {
 
 var newElem = generateElement({
   tag: 'div',
-  classes: 'class1 class2',
+  classList: 'class1 class2',
   id: 'elem_id_1',
   content: 'Go !', // innerHTML
   style: 'background:#ccc',
-  attribs: {
+  attributes: {
     'tabindex': '0',
     'role': 'button',
     'aria-label': 'Validate form'
